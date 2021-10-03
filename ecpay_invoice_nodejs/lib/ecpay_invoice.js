c47c23e78ff6131ef750ab750e241cf3ad98dc5a
Ian Chen <c.cosnaye@gmail.com>
2021-10-03 19:04:23 +0800

    allow passing in config

--- ecpay_invoice_nodejs/lib/ecpay_invoice.js
+++ ecpay_invoice_nodejs/lib/ecpay_invoice.js
@@ -1,17 +1,20 @@
 /**
  * Created by ying.wu on 2017/7/10.
  */
-const version = require('./ecpay_invoice/version.js');
-const invoice_client = require('./ecpay_invoice/invoice_client.js');
-const query_client = require('./ecpay_invoice/query_client.js');
-const notify_client = require('./ecpay_invoice/notify_client.js');
+const version = require("./ecpay_invoice/version.js");
+const invoice_client = require("./ecpay_invoice/invoice_client.js");
+const query_client = require("./ecpay_invoice/query_client.js");
+const notify_client = require("./ecpay_invoice/notify_client.js");
+const helper = require("./ecpay_invoice/helper.js");
 
-class ECPayInvoice{
-    constructor(){
+class ECPayInvoice {
+    constructor() {
+        this.helper = new helper(config);
+
         this.version = new version();
-        this.invoice_client = new invoice_client();
-        this.query_client = new query_client();
-        this.notify_client = new notify_client();
+        this.invoice_client = new invoice_client(this.helper);
+        this.query_client = new query_client(this.helper);
+        this.notify_client = new notify_client(this.helper);
     }
 }
 module.exports = ECPayInvoice;
